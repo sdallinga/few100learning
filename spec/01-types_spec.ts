7501c0cb084d803e02a86d75224f31851a29164b
Scott Dallinga <sdallinga@gmail.com>
2019-02-19 16:38:25 -0500

    End of 2nd Day

--- spec/01-types_spec.ts
+++ spec/01-types_spec.ts
@@ -1,110 +1,103 @@
+import { formatName } from "./utils";
 describe('variables and constants and stuff', () => {
-    describe('declaring variables', () => {
-        it('using let to create a binding', () => {
-            let name;
-            name = 'Joe';
-            expect(name).toBe('Joe');
-            name = 11;
-            expect(name).toBe(11);
-        });
-        it('some typescript stuff for variables', () => {
-            let name: string | number = 'Joe'; // Union type
-            //name = 'Joe';
-            expect(name).toBe('Joe');
-            name = 11;
-            expect(name).toBe(11);
-        });
-        it('declaring constants', () => {
-            const PI = 3.14159;  // You can't assign a new value to this variable
-            const FAVORITE_NUMBERS = [9, 22, 108];
 
-            //FAVORITE_NUMBERS = [];  // illegal
-            FAVORITE_NUMBERS[0] = 10;   // allowed
+	describe('declaring variables', () => {
+		it('using let to create a binding', () => {
+			let name;
+			name = 'Joe';
+			expect(name).toBe('Joe');
+			name = 11;
+			expect(name).toBe(11);
+		});
+		it('some typescript stuff for variables', () => {
+			let name: string | number = 'Joe'; // Union type
+			//name = 'Joe';
+			expect(name).toBe('Joe');
+			name = 11;
+			expect(name).toBe(11);
+		});
+		it('declaring constants', () => {
+			const PI = 3.14159;  // You can't assign a new value to this variable
+			const FAVORITE_NUMBERS = [9, 22, 108];
 
-            const MOVIE = {
-                title: 'The Force Awakens',
-                director: 'Abrams'
-            };
+			//FAVORITE_NUMBERS = [];  // illegal
+			FAVORITE_NUMBERS[0] = 10;   // allowed
 
-            MOVIE.director = 'Johnson';    // Objects are mutable in JavaScript
-        });
+			const MOVIE = {
+				title: 'The Force Awakens',
+				director: 'Abrams'
+			};
 
-        it('var is still there but it stinks and should not be used', () => {
-            if (true) {
-                var name = 'Fido';  // This is the thing you should NOT do (because of JavaScript variable hoisting)
-            }
-            expect(name).toBe('Fido');
-        });
+			MOVIE.director = 'Johnson';    // Objects are mutable in JavaScript
+		});
 
-        describe('strings', () => {
-            it('delimiting', () => {
-                let first = 'Joe',
-                    last = "Schmidt";
-                expect("Joe").toBe(first);
+		it('var is still there but it stinks and should not be used', () => {
+			if (true) {
+				var name = 'Fido';  // This is the thing you should NOT do (because of JavaScript variable hoisting)
+			}
+			expect(name).toBe('Fido');
+		});
+	});
+	describe('strings', () => {
+		it('delimiting', () => {
+			let first = 'Joe',
+				last = "Schmidt";
+			expect("Joe").toBe(first);
 
-                let msg = "She told \"Get Lost!\"";
-                let msg2 = 'She told me "Get Lost!"';
+			let msg = "She told \"Get Lost!\"";
+			let msg2 = 'She told me "Get Lost!"';
 
-                let story = `Chapter 1.
-            
+			let story = `Chapter 1.
+			
 It was a drak and stormy night`;
-                console.log(story);
+			console.log(story);
 
-                let fullName = `That is ${last}, ${first}`;
-                expect(fullName).toBe('That is Schmidt, Joe');
+			let fullName = `That is ${last}, ${first}`;
+			expect(fullName).toBe('That is Schmidt, Joe');
+		});
+	});
 
-            });
+	describe('various literals', () => {
+		it('examples', () => {
+			let n1 = 12 // number
+			let n2 = 1.3 // still a number
+			let n3 = 0xff; // still a number, but hexidecimal
+			let n4 = 0b00101;  // still a number but binary
+			let n5 = 0o744; // octal. Who the heck uses that??
 
-            describe('various literals', () => {
-                it('examples', () => {
-                    let n1 = 12 // number
-                    let n2 = 1.3 // still a number
-                    let n3 = 0xff; // still a number, but hexidecimal
-                    let n4 = 0b00101;  // still a number but binary
-                    let n5 = 0o744; // octal. Who the heck uses that??
+			//typescript thing
+			const salary = 1_000_000;    // you can use this notation for large numbers.
+		});
+	});
 
-                    //typescript thing
-                    const salary = 1_000_000;    // you can use this notation for large numbers.
-                });
-            });
+	describe('arrays and array literals', () => {
+		it('has them', () => {
+			const stuff: Array<number | string> = [12, 13];
+			stuff[2] = 'tacos';
+			expect(stuff[2]).toBe('tacos');
+			let food = stuff[2];
+		});
+		describe('tuples', () => {
+			it('a brief introduction TS', () => {
+				let warren: [string, string, number, string];
+				warren = ['Warren', 'Ellis', 55, 'Musician'];
+				let occupation = warren[3];
+				let age = warren[2];
+			});
+			it('an example of Tuples', () => {
+				function formatName(first: string, last: string): [string, number] {
+					const fullName = `${last}, ${first}`;
+					return [fullName, fullName.length];
+				}
+				const [fullName, len] = formatName('Han', 'Solo');  // destructuring
+				expect(fullName).toBe('Solo, Han');
+				expect(len).toBe(9);
 
-            describe('arrays and array literals', () => {
-                it('has them', () => {
-                    const stuff: Array<number | string> = [12, 13];
-                    stuff[2] = 'tacos';
-                    expect(stuff[2]).toBe('tacos');
-                    let food = stuff[2];
-                });
-            });
-            describe('tuples', () => {
-
-
-                it('a brief introduction TS'), () => {
-                    let warren: [string, string, number, string];
-                    warren = ['Warren', 'Ellis', 55, 'Musician'];
-                    let occupation = warren[3];
-                    let age = warren[2];
-                }
-
-                it('an example of Tuples', () => {
-                    function formatName(first: string, last: string): [string, number] {
-                        const fullName = `${last}, ${first}`;
-                        return [fullName, fullName.length];
-                    }
-
-                    const [fullName, len] = formatName('Han', 'Solo');  // destructuring
-                    expect(fullName).toBe('Solo, Han');
-                    expect(len).toBe(9);
-
-                    const stuff = ['Jeff', 'Gonzalez', 49];
-                    const [firstName, , age] = stuff;
-                    expect(firstName).toBe('Jeff');
-                    expect(age).toBe(49);
-
-                });
-            });
-        });
-
-
-    });
+				const stuff = ['Jeff', 'Gonzalez', 49];
+				const [firstName, , age] = stuff;
+				expect(firstName).toBe('Jeff');
+				expect(age).toBe(49);
+			});
+		});
+	});
 });
--- spec/02-functions_spec.ts
+++ spec/02-functions_spec.ts
@@ -1,35 +1,124 @@
+import { formatName } from "./utils";
+import * as _ from 'lodash';  // this module has pure javascript .d.ts (type definition library)
+
 describe('functions', () => {
-    describe('syntax for creating them', () => {
-        it('declaring them', () => {
-            // Named function - you can forward reference named functions in scope.
-            function add(a: number, b: number) {
-                return a + b;
-            }
-            expect(add(3, 3)).toBe(6);
+	describe('syntax for creating them', () => {
+		it('declaring them', () => {
+			// Named function - you can forward reference named functions in scope.
+			function add(a: number, b: number) {
+				return a + b;
+			}
+			expect(add(3, 3)).toBe(6);
 
-            // Anonymous functions
-            // -- an anonymous function that I immediately invoke
-            expect((function (a, b) { return a / b; })(10, 5)).toBe(2);
+			// Anonymous functions
+			// -- an anonymous function that I immediately invoke
+			expect((function (a, b) { return a / b; })(10, 5)).toBe(2);
 
-            const multiply = function (a: number, b: number) { return a * b; };
-            expect(multiply(3, 3)).toBe(9);
+			const multiply = function (a: number, b: number) { return a * b; };
+			expect(multiply(3, 3)).toBe(9);
 
-            const divide = (a: number, b: number) => a / b;     // 'fat arrow' means 'goes to ' in english
-            // no return needed when body of function is an expression
-            expect(divide(10, 2)).toBe(5);
+			const divide = (a: number, b: number) => a / b;     // 'fat arrow' means 'goes to ' in english
+			// no return needed when body of function is an expression
+			expect(divide(10, 2)).toBe(5);
 
-            const logIt = (msg: string) => {
-                msg = msg.toUpperCase();
-                console.log(msg);
-                return true;
-            };
-            logIt('Fun with functions!');
-        });
-    });
-    describe('higher order function', () => {
-        // Higher order function: function that takes one or more functions as arguments and returns a function as a result.
-        it('first example', () => {
-        });
-    });
+			const logIt = (msg: string) => {
+				msg = msg.toUpperCase();
+				console.log(msg);
+				return true;
+			};
+			logIt('Fun with functions!');
+		});
+	});
+	describe('higher order function', () => {
+		// Higher order function: function that takes one or more functions as arguments and returns a function as a result.
+		it('first example', () => {
+			const answer = formatName('Han', 'Solo', makeItUpper);
+			expect(answer).toBe('SOLO, HAN');
 
+			const answer2 = formatName('Han', 'Solo', (x: string) => `***${x}***`);
+			expect(answer2).toBe('***Solo, Han***');
+
+			function makeItUpper(what: string) {
+				return what.toUpperCase();
+			}
+
+		});
+
+		describe('a function that returns a function', () => {
+			it('a way to do it that doesn\'t seem crazy', () => {
+				// <h1>Hello</h1>
+				//  tag, content
+				// straight ahead procedural function type thing.
+				function makeElement(tag: string, content: string) {
+					return `<${tag}>${content}</${tag}>`;
+				}
+				expect(makeElement('h1', 'Hello')).toBe('<h1>Hello</h1>');
+				expect(makeElement('p', 'the story')).toBe('<p>the story</p>');
+			});
+			it('you could try object oriented programming', () => {
+				class ElementMaker {
+					tag: string;
+					constructor(tag: string) {
+						this.tag = tag;
+					}
+
+					make(content: string) {
+						return `<${this.tag}>${content}</${this.tag}>`;
+					}
+				}
+
+				const h1Maker = new ElementMaker('h1');
+				expect(h1Maker.make('Hello')).toBe('<h1>Hello</h1>');
+				expect(h1Maker.make('Big!')).toBe('<h1>Big!</h1>');
+			});
+			it('a higher-order function version', () => {
+				function tagMaker(tag: string) {
+					return (content: string) => `<${tag}>${content}</${tag}>`
+				}
+				const h1Maker = tagMaker('h1');
+				const pMaker = tagMaker('p');
+
+				expect(h1Maker('Big!')).toBe('<h1>Big!</h1>');
+				expect(pMaker('small')).toBe('<p>small</p>');
+			});
+		});
+	});
+});
+describe('some lodash goodness', () => {
+	it('supports memoization', () => {
+		function doSomeHardWork() {
+			console.log('Doing hard work here!');
+			return 'Work Is Done!';
+		}
+		const memoized = _.memoize(doSomeHardWork);
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+		expect(memoized()).toBe('Work Is Done!');
+	});
+	it('currying', () => {
+		function makeElement(tag: string, content: string) {
+			return `<${tag}>${content}</${tag}>`;
+		}
+
+		const curriedTagMaker = _.curry(makeElement);
+		const h1Maker = curriedTagMaker('h1');
+		const pMaker = curriedTagMaker('p');
+		expect(h1Maker('Hello')).toBe('<h1>Hello</h1>');
+		expect(pMaker('see you tomorrow')).toBe('<p>see you tomorrow</p>');
+		function add(a: number, b: number, c: number) {
+			return a + b + c;
+		}
+		const curriedAdd = _.curry(add);
+		const f1 = curriedAdd(10, 5);
+		expect(f1(1)).toBe(16);
+	});
 });
